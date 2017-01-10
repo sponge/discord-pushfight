8308469ac3fa76248913f1816510f724ee5372b0
sponge <sponge@d8d.org>
2017-01-10 16:40:10 -0600

    redo board and more rendering cleanups

--- ImageRenderer/ImageRenderer.cs
+++ ImageRenderer/ImageRenderer.cs
@@ -19,15 +19,18 @@ namespace ImageRenderer
         Image Anchor = new Image(File.ReadAllBytes("img/anchor.png"));
         Image Arrow = new Image(File.ReadAllBytes("img/arrow.png"));
 
+        private int cellSize = 90;
+        private int cellInnerSize = 84;
+
         private Point ToPixels(Point cell)
         {
-            return new Point((int)(88.5 * cell.X), (int)(91 * cell.Y) - 23); ;
+            return new Point((int)(cellSize * (cell.X - 1)) + 76, (int)(cellSize * (cell.Y - 1)) + 82);
         }
 
         private void DrawHighlight(PushFightGame game, Image output, Point cell)
         {
             var px = ToPixels(cell);
-            output.Fill(new Color(255, 255, 0, 90), new RectangularPolygon(new Rectangle(px.X, px.Y, 88, 91)));
+            output.Fill(new Color(255, 255, 0, 90), new RectangularPolygon(new Rectangle(px.X, px.Y, cellInnerSize, cellInnerSize)));
         }
 
         private void DrawPath(PushFightGame game, Image output, Point startCell, Point endCell) {
@@ -45,6 +48,25 @@ namespace ImageRenderer
             var arr = new Image(Arrow);
             arr.Rotate(dir == Direction.Right ? 0 : dir == Direction.Down ? 90 : dir == Direction.Left ? 180 : 270);
             output.DrawImage(arr, 100, new Size(arr.Width, arr.Height), px);
+        }
+
+        private void DrawPiece(PushFightGame game, Image output, Point cell, Pawn pawn) {
+            Image img;
+            if (pawn.Team == Team.None)
+            {
+                return;
+            }
+            else if (pawn.Team == Team.Black)
+            {
+                img = pawn.Type == PawnType.Round ? BlackRound : BlackSquare;
+            }
+            else
+            {
+                img = pawn.Type == PawnType.Round ? WhiteRound : WhiteSquare;
+            }
+
+            var px = ToPixels(cell);
+            output.DrawImage(img, 100, new Size(img.Width, img.Height), new Point(px.X, px.Y));
         }
 
 
@@ -78,33 +100,17 @@ namespace ImageRenderer
                 {
                     var cell = game.Board[x, y];
 
-                    var pos = ToPixels(new Point(x, y));
-
                     if (cell.Highlight)
                     {
                         DrawHighlight(game, output, new Point(x, y));
                     }
 
-                    if (cell.Contents.Type == PawnType.Empty)
-                    {
-                        continue;
-                    }
-
-                    Image img;
-                    if (cell.Contents.Team == Team.Black)
-                    {
-                        img = cell.Contents.Type == PawnType.Round ? BlackRound : BlackSquare;
-                    }
-                    else
-                    {
-                        img = cell.Contents.Type == PawnType.Round ? WhiteRound : WhiteSquare;
-                    }
-
-                    output.DrawImage(img, 100, new Size(img.Width, img.Height), new Point(pos.X + 4, pos.Y));
+                    DrawPiece(game, output, new Point(x, y), cell.Contents);
 
                     if (cell.Anchored)
                     {
-                        output.DrawImage(Anchor, 100, new Size(Anchor.Width, Anchor.Height), new Point(pos.X + 17, pos.Y + 13));
+                        var pos = ToPixels(new Point(x, y));
+                        output.DrawImage(Anchor, 100, new Size(Anchor.Width, Anchor.Height), new Point(pos.X, pos.Y));
                     }
                 }
             }
