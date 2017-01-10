2f00cb5eca6a2722d2e1e89e765bfaa34c9b8ea0
sponge <sponge@d8d.org>
2017-01-09 19:30:22 -0600

    draw placements/pawn moves/pawn pushes

--- ImageRenderer/ImageRenderer.cs
+++ ImageRenderer/ImageRenderer.cs
@@ -5,6 +5,7 @@ using ImageSharp;
 using PushFight;
 using System.IO;
 using ImageSharp.Drawing.Shapes;
+using System.Numerics;
 
 namespace ImageRenderer
 {
@@ -16,12 +17,40 @@ namespace ImageRenderer
         Image WhiteRound = new Image(File.ReadAllBytes("img/wr.png"));
         Image WhiteSquare = new Image(File.ReadAllBytes("img/ws.png"));
         Image Anchor = new Image(File.ReadAllBytes("img/anchor.png"));
+        Image Arrow = new Image(File.ReadAllBytes("img/arrow.png"));
+
 
         public MemoryStream Render(PushFightGame game)
         {
             var output = new Image(Board.Width, Board.Height);
 
             output.DrawImage(Board, 100, new Size(Board.Width, Board.Height), new Point(0, 0));
+
+            foreach (var ev in game.LastTurnEvents)
+            {
+                var pos1 = new Point((int)(88.5 * ev.x1), (int)(91 * ev.y1) - 23);
+                var pos2 = new Point((int)(88.5 * ev.x2), (int)(91 * ev.y2) - 23);
+
+                switch (ev.Type)
+                {
+                    case TurnEventType.Place:
+                        output.Fill(new Color(255, 255, 0, 90), new RectangularPolygon(new Rectangle(pos1.X, pos1.Y, 88, 91)));
+                        break;
+
+                    case TurnEventType.Move:
+                        output.DrawLines(new ImageSharp.Drawing.Pens.Pen(Color.Black, 2.0f), new[] {
+                            new Vector2(pos1.X + 30, pos1.Y + 30),
+                            new Vector2(pos2.X + 30, pos2.Y + 30)
+                        });
+                        break;
+
+                    case TurnEventType.Push:
+                        var arr = new Image(Arrow);
+                        arr.Rotate(ev.Direction == Direction.Right ? 0 : ev.Direction == Direction.Down ? 90 : ev.Direction == Direction.Left ? 180 : 270);
+                        output.DrawImage(arr, 100, new Size(arr.Width, arr.Height), pos1);
+                        break;
+                }
+            }
 
             for (int x = 0; x < game.Board.GetLength(0); x++)
             {
